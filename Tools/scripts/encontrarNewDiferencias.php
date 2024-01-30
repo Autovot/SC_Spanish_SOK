47a3049f089e2055f90f85046809a899902ccc84
Autovot <87210193+Autovot@users.noreply.github.com>
2024-01-30 23:43:59 +0100

    Add new files and update .gitignore

---
+++ Tools/scripts/encontrarNewDiferencias.php
@@ -0,0 +1,13 @@
+<?php
+$archivoOriginal = file(__DIR__ . "/../../data/Localization/english/global.ini");
+$archivoNuevo = file(__DIR__ . "/../../data/Localization/english/global.new.ini");
+foreach ($archivoOriginal as $numLinea => $contenidoLinea) {
+    if ($contenidoLinea != $archivoNuevo[$numLinea]) {
+        echo "\n---------------------------------------------";
+        echo "\nLa línea " . ($numLinea + 1) . " no coincide\n";
+        echo "ORIGINAL     : \033[0;31m" . $contenidoLinea . "\033[0m\n";
+        echo "TRADUCCIÓN   : \033[0;31m" . $$archivoNuevo[$numLinea] . "\033[0m";
+        echo "\n---------------------------------------------\n\n";
+        break;
+    }
+}
