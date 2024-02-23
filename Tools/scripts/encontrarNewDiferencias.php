5e23fafb4872c3a3e384d2cc67393abf71e31770
Autovot <87210193+Autovot@users.noreply.github.com>
2024-02-23 23:37:27 +0100

    Added 3.22.1 .ini file and updated script

--- Tools/scripts/encontrarNewDiferencias.php
+++ Tools/scripts/encontrarNewDiferencias.php
@@ -5,8 +5,8 @@ foreach ($archivoOriginal as $numLinea => $contenidoLinea) {
     if ($contenidoLinea != $archivoNuevo[$numLinea]) {
         echo "\n---------------------------------------------";
         echo "\nLa línea " . ($numLinea + 1) . " no coincide\n";
-        echo "ORIGINAL     : \033[0;31m" . $contenidoLinea . "\033[0m\n";
-        echo "Nuevo   : \033[0;31m" . $archivoNuevo[$numLinea] . "\033[0m";
+        echo "ORIGINAL : \033[0;31m" . $contenidoLinea . "\033[0m\n";
+        echo "Nuevo    : \033[0;31m" . $archivoNuevo[$numLinea] . "\033[0m";
         echo "\n---------------------------------------------\n\n";
         break;
     }
