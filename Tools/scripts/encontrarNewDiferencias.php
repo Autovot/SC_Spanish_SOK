e84e4f5d89d1db55db28ef1db0c462e0ad2a84c5
zzettazz <junquerdaviddaw@gmail.com>
2024-04-11 16:04:38 +0200

    update diferencias tool

--- Tools/scripts/encontrarNewDiferencias.php
+++ Tools/scripts/encontrarNewDiferencias.php
@@ -5,8 +5,8 @@ foreach ($archivoOriginal as $numLinea => $contenidoLinea) {
     if ($contenidoLinea != $archivoNuevo[$numLinea]) {
         echo "\n---------------------------------------------";
         echo "\nLa línea " . ($numLinea + 1) . " no coincide\n";
-        echo "ORIGINAL : \033[0;31m" . $contenidoLinea . "\033[0m\n";
-        echo "Nuevo    : \033[0;31m" . $archivoNuevo[$numLinea] . "\033[0m";
+        echo "\033[0;33m[ORIGINAL]\033[0m : \033[0;31m" . $contenidoLinea . "\033[0m\n";
+        echo "\033[0;36m[NUEVO]\033[0m    : \033[0;31m" . $archivoNuevo[$numLinea] . "\033[0m";
         echo "\n---------------------------------------------\n\n";
         break;
     }
