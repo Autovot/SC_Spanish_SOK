a1db9b97f3c992cdefda5cc46ddd70f1fb96ed65
zzettazz <junquerdaviddaw@gmail.com>
2024-04-11 17:30:01 +0200

    update script

--- Tools/scripts/encontrarNewDiferencias.php
+++ Tools/scripts/encontrarNewDiferencias.php
@@ -1,4 +1,6 @@
 <?php
+$error = false;
+
 $archivoOriginal = file(__DIR__ . "/../../data/Localization/english/global.ini");
 $archivoNuevo = file(__DIR__ . "/../../data/Localization/english/global.new.ini");
 foreach ($archivoOriginal as $numLinea => $contenidoLinea) {
@@ -8,6 +10,15 @@ foreach ($archivoOriginal as $numLinea => $contenidoLinea) {
         echo "\033[0;33m[ORIGINAL]\033[0m : \033[0;31m" . $contenidoLinea . "\033[0m\n";
         echo "\033[0;36m[NUEVO]\033[0m    : \033[0;31m" . $archivoNuevo[$numLinea] . "\033[0m";
         echo "\n---------------------------------------------\n\n";
+
+        $error = true;
         break;
     }
+}
+
+if (!$error)
+{
+    echo "\n---------------------------------------------\n";
+    echo "\033[0;32m¡¡Enhorabuena!!\033[0m, has terminado con las diferencias.";
+    echo "\n---------------------------------------------\n\n";
 }
